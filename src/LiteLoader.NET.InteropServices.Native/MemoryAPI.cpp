6ff567443d4617aa1ad39901dca9bb85501cd120
Pd233 <minecraftpd233@163.com>
2023-01-30 23:31:55 +0800

    Fix bulid

--- src/LiteLoader.NET.InteropServices.Native/MemoryAPI.cpp
+++ src/LiteLoader.NET.InteropServices.Native/MemoryAPI.cpp
@@ -1,1 +1,87 @@
-#include <memory>namespace LiteLoader::NET::InteropServices::Native{    public ref class MemoryAPI abstract sealed    {    public:        static void* operator_new(size_t size)        {            return ::operator new(size);        }        static void operator_delete(void* ptr)        {            ::operator delete(ptr);        }        static void operator_delete(void** ptr)        {            if ((*ptr) == nullptr)                return;            else            {                ::operator delete(*ptr);                *ptr = nullptr;            }        }        static void operator_delete_array(void* ptr)        {            ::operator delete[](ptr);        }        static void operator_delete_array(void** ptr)        {            if ((*ptr) == nullptr)                return;            else            {                ::operator delete[](*ptr);                *ptr = nullptr;            }        }    };}
+#include <memory>
+#include <Windows.h>
+
+#pragma unmanaged
+
+typedef unsigned long EXCEPTION_CODE;
+
+EXCEPTION_CODE __delete(void** ptr)
+{
+    __try
+    {
+        ::operator delete(*ptr);
+        *ptr = nullptr;
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        return GetExceptionCode();
+    }
+    return 0;
+}
+
+EXCEPTION_CODE __delete_array(void** ptr)
+{
+    __try
+    {
+        ::operator delete[](*ptr);
+        *ptr = nullptr;
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        return GetExceptionCode();
+    }
+    return 0;
+}
+
+#pragma managed
+
+namespace LiteLoader::NET::InteropServices::Native
+{
+    public ref class MemoryAPI abstract sealed
+    {
+    public:
+        static void* operator_new(size_t size)
+        {
+            try
+            {
+                return ::operator new(size);
+            }
+            catch (const std::bad_alloc&)
+            {
+                throw gcnew System::OutOfMemoryException();
+            }
+        }
+
+        static EXCEPTION_CODE operator_delete(void** ptr)
+        {
+            if (ptr == nullptr)
+                return 0;
+
+            if (*ptr == nullptr)
+                return 0;
+
+            return  __delete(ptr);
+        }
+
+        static EXCEPTION_CODE operator_delete(void* ptr)
+        {
+            return operator_delete(&ptr);
+        }
+        
+        static EXCEPTION_CODE operator_delete_array(void** ptr)
+        {
+            if (ptr == nullptr)
+                return 0;
+
+            if (*ptr == nullptr)
+                return 0;
+
+            return  __delete_array(ptr);
+        }
+        
+        static EXCEPTION_CODE operator_delete_array(void* ptr)
+        {
+            return operator_delete_array(&ptr);
+        }
+    };
+}
