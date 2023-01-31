28ce6559698caa22f7c982200253f2a215941149
Pd233 <minecraftpd233@163.com>
2023-01-31 23:39:45 +0800

    Create gsl_not_null.cs

---
+++ src/LiteLoader.NET.InteropServices/gsl_not_null.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace LiteLoader.NET.InteropServices.Gsl;
+
+[StructLayout(LayoutKind.Explicit, Pack = 1, Size = 8)]
+public unsafe struct not_null<T> : IConstructableCppClass<not_null<T>> where T : IConstructableCppClass<T>, new()
+{
+    [FieldOffset(0)]
+    private nint ptr;
+
+    internal not_null(nint p)
+    {
+        ptr = p;
+    }
+
+    public nint NativePointer { get => ptr; set => ptr = value; }
+    public bool OwnsNativeInstance { get { return true; } set { return; } }
+
+    public static implicit operator not_null<T>(T val)
+    {
+        return new not_null<T>(val.NativePointer);
+    }
+
+    public static explicit operator T(not_null<T> val)
+    {
+        return val.Dereference();
+    }
+
+    public not_null<T> ConstructInstance(nint ptr, bool ownsInstance)
+    {
+        var ret = default(not_null<T>);
+        ret.ptr = ptr;
+        return ret;
+    }
+    public void Destruct()
+    {
+        return;
+    }
+
+    public ulong GetClassSize()
+    {
+        return 8;
+    }
+
+    public void SetNativePointer(nint ptr, bool ownsInstance)
+    {
+        this.ptr = ptr;
+    }
+
+    private static T _EmptyTval = default!;
+
+    public T Dereference()
+    {
+        if (ICppClassHelper<T>.isValueType)
+        {
+            if (ICppClassHelper<T>.isICppClass)
+            {
+                return ICppClassHelper<T>._Value_type_funcptr_def.construct_instance(ref _EmptyTval, Unsafe.Read<nint>(ptr.ToPointer()), false);
+            }
+            return Unsafe.Read<T>(ptr.ToPointer());
+        }
+        else
+        {
+            return ICppClassHelper<T>._Ref_type_funcptr_def.construct_instance(_EmptyTval, Unsafe.Read<nint>(ptr.ToPointer()), false);
+        }
+    }
+}
