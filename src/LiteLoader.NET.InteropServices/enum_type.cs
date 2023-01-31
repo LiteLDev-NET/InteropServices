19311ae8b2c15f6edbbf1952392a9912c74e6083
Pd233 <minecraftpd233@163.com>
2023-01-31 18:42:47 +0800

    Create enum_type.cs

---
+++ src/LiteLoader.NET.InteropServices/enum_type.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteLoader.NET.InteropServices;
+
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct enum_type<T> where T : struct, Enum
+{
+    private T value;
+
+    private enum_type(T value)
+    {
+        this.value = value;
+    }
+
+    public T Value { get => value; set => this.value = value; }
+
+    public static implicit operator enum_type<T>(T value)
+    {
+        return new(value);
+    }
+
+    public static explicit operator T(enum_type<T> value)
+    {
+        return value.value;
+    }
+}
