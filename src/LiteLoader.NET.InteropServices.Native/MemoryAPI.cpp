4ccb37be9b1a5203b22e00e9fdb729d3c21c97e5
Pd233 <minecraftpd233@163.com>
2023-03-10 12:31:27 +0800

    Update

--- src/LiteLoader.NET.InteropServices.Native/MemoryAPI.cpp
+++ src/LiteLoader.NET.InteropServices.Native/MemoryAPI.cpp
@@ -1,9 +1,33 @@
 #include <memory>
 #include <Windows.h>
 
-#pragma unmanaged
-
 typedef unsigned long EXCEPTION_CODE;
+
+EXCEPTION_CODE __new(void*& ptr, size_t size)
+{
+    __try
+    {
+        ptr = ::operator new(size);
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        return GetExceptionCode();
+    }
+    return 0;
+}
+
+EXCEPTION_CODE __new_array(void*& ptr, size_t size)
+{
+    __try
+    {
+        ptr = ::operator new[](size);
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        return GetExceptionCode();
+    }
+    return 0;
+}
 
 EXCEPTION_CODE __delete(void** ptr)
 {
@@ -19,12 +43,12 @@ EXCEPTION_CODE __delete(void** ptr)
     return 0;
 }
 
-EXCEPTION_CODE __delete_with_size(void** ptr, size_t size)
+EXCEPTION_CODE __delete_with_size(void*& ptr, size_t size)
 {
     __try
     {
-        ::operator delete(*ptr, size);
-        *ptr = nullptr;
+        ::operator delete(ptr, size);
+        ptr = nullptr;
     }
     __except (EXCEPTION_EXECUTE_HANDLER)
     {
@@ -33,12 +57,12 @@ EXCEPTION_CODE __delete_with_size(void** ptr, size_t size)
     return 0;
 }
 
-EXCEPTION_CODE __delete_array(void** ptr)
+EXCEPTION_CODE __delete_array(void*& ptr)
 {
     __try
     {
-        ::operator delete[](*ptr);
-        *ptr = nullptr;
+        ::operator delete[](ptr);
+        ptr = nullptr;
     }
     __except (EXCEPTION_EXECUTE_HANDLER)
     {
@@ -47,76 +71,38 @@ EXCEPTION_CODE __delete_array(void** ptr)
     return 0;
 }
 
-#pragma managed
-
-namespace LiteLoader::NET::InteropServices::Native
+extern "C"
 {
-    public ref class MemoryAPI abstract sealed
+    __declspec(dllexport) EXCEPTION_CODE MemoryAPI_operator_new(void*& ptr, size_t size)
     {
-    public:
-        static void* operator_new(size_t size)
-        {
-            try
-            {
-                return ::operator new(size);
-            }
-            catch (const std::bad_alloc&)
-            {
-                throw gcnew System::OutOfMemoryException();
-            }
-        }
+        return __new(ptr, size);
+    }
+    
+    __declspec(dllexport) EXCEPTION_CODE MemoryAPI_operator_new_array(void*& ptr, size_t size)
+    {
+        return __new_array(ptr, size);
+    }
 
-        static EXCEPTION_CODE operator_delete(void** ptr)
-        {
-            if (ptr == nullptr)
-                return 0;
+    __declspec(dllexport) EXCEPTION_CODE MemoryAPI_operator_delete(void* ptr)
+    {
+        return __delete(&ptr);
+    }
 
-            if (*ptr == nullptr)
-                return 0;
+    __declspec(dllexport) EXCEPTION_CODE MemoryAPI_operator_delete_with_size(void* ptr, size_t size)
+    {
+        if (ptr == nullptr)
+            return 0;
 
-            return  __delete(ptr);
-        }
+        return  __delete_with_size(ptr, size);
+    }
 
-        static EXCEPTION_CODE operator_delete(void* ptr)
-        {
-            return operator_delete(&ptr);
-        }
+    __declspec(dllexport) EXCEPTION_CODE MemoryAPI_operator_delete_array(void* ptr)
+    {
+        return __delete_array(ptr);
+    }
 
-        static EXCEPTION_CODE operator_delete(void** ptr, size_t size)
-        {
-            if (ptr == nullptr)
-                return 0;
-
-            if (*ptr == nullptr)
-                return 0;
-
-            return  __delete_with_size(ptr, size);
-        }
-
-        static EXCEPTION_CODE operator_delete(void* ptr, size_t size)
-        {
-            return operator_delete(&ptr, size);
-        }
-
-        static EXCEPTION_CODE operator_delete_array(void** ptr)
-        {
-            if (ptr == nullptr)
-                return 0;
-
-            if (*ptr == nullptr)
-                return 0;
-
-            return  __delete_array(ptr);
-        }
-
-        static EXCEPTION_CODE operator_delete_array(void* ptr)
-        {
-            return operator_delete_array(&ptr);
-        }
-
-        static void memmove(void* dst, void* src, size_t size)
-        {
-            ::memmove(dst, src, size);
-        }
-    };
+    __declspec(dllexport) void MemoryAPI_memmove(void* dst, void* src, size_t size)
+    {
+        ::memmove(dst, src, size);
+    }
 }
---
+++ src/LiteLoader.NET.InteropServices/MemoryAPI.cs
@@ -0,0 +1,96 @@
+﻿using System.Runtime.InteropServices;
+using System.Security;
+using EXCEPTION_CODE = System.UInt64;
+using size_t = System.UInt64;
+
+namespace LiteLoader.NET.InteropServices.Native
+{
+    public unsafe static class MemoryAPI
+    {
+        [DllImport("LiteLoader.NET.InteropServices.Native.dll",
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "MemoryAPI_operator_new",
+            SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern EXCEPTION_CODE internal_operator_new(out void* ptr, size_t size);
+
+        [DllImport("LiteLoader.NET.InteropServices.Native.dll",
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "MemoryAPI_operator_new_array",
+            SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern EXCEPTION_CODE internal_operator_new_array(out void* ptr, size_t size);
+
+        [DllImport("LiteLoader.NET.InteropServices.Native.dll",
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "MemoryAPI_operator_delete",
+            SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern EXCEPTION_CODE internal_operator_delete(void* ptr);
+
+        [DllImport("LiteLoader.NET.InteropServices.Native.dll",
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "MemoryAPI_operator_delete_with_size",
+            SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern EXCEPTION_CODE internal_operator_delete_with_size(void* ptr, size_t size);
+
+        [DllImport("LiteLoader.NET.InteropServices.Native.dll",
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "MemoryAPI_operator_delete_array",
+            SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern EXCEPTION_CODE internal_operator_delete_array(void* ptr);
+
+        [DllImport("LiteLoader.NET.InteropServices.Native.dll",
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "MemoryAPI_memmove",
+            SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern EXCEPTION_CODE internal_memmove(void* dst, void* src, size_t size);
+
+        public static void* operator_new(size_t size)
+        {
+            if (internal_operator_new(out var ptr, size) == 0)
+            {
+                return ptr;
+            }
+
+            throw new OutOfMemoryException();
+        }
+
+        public static void* operator_new_array(size_t size)
+        {
+            if (internal_operator_new(out var ptr, size) == 0)
+            {
+                return ptr;
+            }
+
+            throw new OutOfMemoryException();
+        }
+
+        public static void operator_delete(void* ptr)
+        {
+            if (internal_operator_delete(ptr) != 0)
+                throw new MemoryCorruptedException(string.Empty);
+        }
+
+        public static void operator_delete_with_size(void* ptr, size_t size)
+        {
+            if (internal_operator_delete_with_size(ptr, size) != 0)
+                throw new MemoryCorruptedException(string.Empty);
+        }
+
+        public static void operator_delete_array(void* ptr)
+        {
+            if (internal_operator_delete_array(ptr) != 0)
+                throw new MemoryCorruptedException(string.Empty);
+        }
+
+        public static void operator_memmove(void* dst, void* src, size_t size)
+        {
+            if (internal_memmove(dst, src, size) != 0)
+                throw new MemoryCorruptedException(string.Empty);
+        }
+    }
+}
